94e5b9d8cc2cafaa902198aef77b75c94c6c9190
Orvlyn <RoadEditor17@gmail.com>
2026-09-11 00:24:28 +1000

    Refactor WatchLaterController for improved structure

--- src/Jellyfin.Plugin.DinkFlixWatchLater/Api/WatchLaterController.cs
+++ src/Jellyfin.Plugin.DinkFlixWatchLater/Api/WatchLaterController.cs
@@ -6,7 +6,7 @@ using Jellyfin.Plugin.DinkFlixWatchLater.Api.Dto;
 using Jellyfin.Plugin.DinkFlixWatchLater.Services;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Library;
-using MediaBrowser.Model.Entities;
+using Jellyfin.Data.Enums;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
