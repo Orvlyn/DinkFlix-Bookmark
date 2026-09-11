08de053a86228c8a34906976c4a25374b1534ae0
Orvlyn <RoadEditor17@gmail.com>
2026-09-11 12:06:04 +1000

    Delete src/Jellyfin.Plugin.DinkFlixWatchLater directory

--- src/Jellyfin.Plugin.DinkFlixWatchLater/PluginConfiguration.cs
+++
@@ -1,2 +0,0 @@
-using MediaBrowser.Model.Plugins;namespace Jellyfin.Plugin.DinkFlixWatchLater;
-public class PluginConfiguration:BasePluginConfiguration{public string SeerrUrl{get;set;}="";public string SeerrApiKey{get;set;}="";}
--- src/Jellyfin.Plugin.DinkFlixWatchLater/PluginServiceRegistrator.cs
+++
@@ -1,3 +0,0 @@
-using Jellyfin.Plugin.DinkFlixWatchLater.Services;using MediaBrowser.Controller;using MediaBrowser.Controller.Plugins;using Microsoft.Extensions.DependencyInjection;
-namespace Jellyfin.Plugin.DinkFlixWatchLater;
-public class PluginServiceRegistrator:IPluginServiceRegistrator{public void RegisterServices(IServiceCollection s,IServerApplicationHost h){s.AddHttpClient();s.AddSingleton<WatchLaterStore>();s.AddSingleton<SeerrClient>();}}
