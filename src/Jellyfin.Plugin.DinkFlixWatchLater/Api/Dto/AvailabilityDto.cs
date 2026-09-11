ca0a7c7115b30702c384397a4fa81765151450b9
Orvlyn <RoadEditor17@gmail.com>
2026-09-11 13:43:12 +1000

    Delete src/Jellyfin.Plugin.DinkFlixWatchLater directory

--- src/Jellyfin.Plugin.DinkFlixWatchLater/Api/Dto/AvailabilityDto.cs
+++
@@ -1,6 +0,0 @@
-namespace Jellyfin.Plugin.DinkFlixWatchLater.Api.Dto;
-public sealed class AvailabilityDto
-{
-    public bool AvailableOnJellyfin { get; set; }
-    public string? JellyfinItemId { get; set; }
-}
--- src/Jellyfin.Plugin.DinkFlixWatchLater/PluginConfiguration.cs
+++
@@ -1,7 +0,0 @@
-using MediaBrowser.Model.Plugins;
-namespace Jellyfin.Plugin.DinkFlixWatchLater;
-public sealed class PluginConfiguration : BasePluginConfiguration
-{
-    public bool EnableHomeIntegration { get; set; } = true;
-    public bool EnableSeerrButtons { get; set; } = true;
-}
